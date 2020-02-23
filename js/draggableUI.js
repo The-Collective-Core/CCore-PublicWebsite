21408e9f15449b32ade34459d82b4f3c9c34ff72
timhow38 <timhow38@gmail.com>
2020-02-24 02:47:26 +1300

    Update

---
+++ js/draggableUI.js
@@ -0,0 +1,45 @@
+/* Menu Show/Hide */
+$( function() {
+    $( "#draggable-JS-00" ).draggable({ snap: true, containment: "body", scroll: false });
+    $( "#draggable-JS-01" ).draggable({ snap: true, containment: "body", scroll: false });
+    $( "#draggable-JS-02" ).draggable({ snap: true, containment: "body", scroll: false });
+  } );
+
+   $(document).ready( function() {
+    $("#draggable-JS-00").show()
+    $("#draggable-JS-01").show()
+    $("#draggable-JS-02").show()
+  }); 
+
+  $(document).ready( function() {
+      $("#button00").on("click", function() {
+          $("#draggable-JS-00").show();
+      });
+  });
+  $(document).ready( function() {
+      $("#close00").on("click", function() {
+          $("#draggable-JS-00").hide();
+      });
+  });
+
+  $(document).ready( function() {
+      $("#button01").on("click", function() {
+          $("#draggable-JS-01").show();
+      });
+  });
+  $(document).ready( function() {
+      $("#close01").on("click", function() {
+          $("#draggable-JS-01").hide();
+      });
+  });
+
+  $(document).ready( function() {
+      $("#button02").on("click", function() {
+          $("#draggable-JS-02").show();
+      });
+  });
+  $(document).ready( function() {
+      $("#close02").on("click", function() {
+          $("#draggable-JS-02").hide();
+      });
+  });
