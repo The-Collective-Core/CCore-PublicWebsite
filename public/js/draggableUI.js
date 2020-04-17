c9bdec81e3423e1a2c32fe3ba1911e52973ab69b
Timothy <35541279+timhow38@users.noreply.github.com>
2020-04-18 04:31:29 +1200

    Update

--- public/js/draggableUI.js
+++ public/js/draggableUI.js
@@ -2,13 +2,13 @@
 $(function () {
 	//CoreCLI
 	$("#draggable-JS-08").draggable({
-		containment: ".folder1",
+		containment: ".sub-folder",
 		scroll: false,
 		grid: [20, 20],
 	});
 	//CoreCLI
 	$("#draggable-JS-09").draggable({
-		containment: ".folder1",
+		containment: ".sub-folder",
 		scroll: false,
 		grid: [20, 20],
 	});
